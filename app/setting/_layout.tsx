4e831814ee72c8c0099a040c48cd52e551a77a47
Banti4750 <bantikumar6203818460@gmail.com>
2025-09-04 15:49:54 +0530

    added setting page

--- app/setting/_layout.tsx
+++ app/setting/_layout.tsx
@@ -1,7 +1,10 @@
-import { Children } from "react";
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
     return (
-        { Children }
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack screenOptions={{ headerShown: false }} />
+        </GestureHandlerRootView>
     );
 }
